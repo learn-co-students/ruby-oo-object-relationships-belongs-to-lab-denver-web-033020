2d0a27a2d7d4fd392d761afb92d6ce4e045d5ef1
LisaHJung <lisahjung@gmail.com>
2020-04-06 13:14:59 -0600

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,3 +2,6 @@ class Song
     attr_accessor :title, :artist
 end 
 
+# class song should belong to an artist.
+# create a method called called artist
+# crete a class called Artist and attr_accessor :name for that artist. 
